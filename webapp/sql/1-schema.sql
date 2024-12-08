8ce5b6ab91cf199d9f222ecc1dc5e616fd06ae3b
muttsu-623 <mutsumi1996@gmail.com>
2024-12-08 03:43:44 +0000

    created_atのindexを追加

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -50,6 +50,7 @@ CREATE TABLE chair_locations
 )
   COMMENT = '椅子の現在位置情報テーブル';
 ALTER TABLE chair_locations ADD INDEX idx_chair_id (chair_id);
+ALTER TABLE chair_locations ADD INDEX idx_created_at (created_at);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
